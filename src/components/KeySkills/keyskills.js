02448cf93c7cfabe6951ac75b37468a6b35114a4
Harish <harishprvn@gmail.com>
2024-11-08 12:29:50 +0530

    style changes for keyskills

--- src/components/KeySkills/keyskills.js
+++ src/components/KeySkills/keyskills.js
@@ -19,45 +19,44 @@ const images = [
 const KeySkills = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    // Handle clicking the next button
-    const handleNext = () => {
+  // Automatically move to the next image every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    };
-    return (
-        <section id='keyskills' className='keyskills'>
-            <div className='keyskillsSection'>
-                <h1 className='ksHeading'>Key Skills</h1>
-                <h1 className='ksHeading2'>Key<span className='ksHeadingSkills'> Skills</span></h1>
-            </div>
+    }, 3000);
 
-            <div className="carousel-container">
-                {/* Image carousel */}
-                <div className="carousel">
-                    <img
-                    src={images[currentIndex]}
-                    alt={`Slide ${currentIndex + 1}`}
-                    className="carousel-image"
-                    />
-                </div>
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(interval);
+  }, []);
 
-                {/* Swipe button */}
-                <button className="swipe-button" onClick={handleNext}>
-                    Swipe →
-                </button>
+  return (
+    <section id='keyskills' className='keyskills'>
+        <div className='keyskillsSection'>
+            <h1 className='ksHeading'>Key Skills</h1>
+            <h1 className='ksHeading2'>Key<span className='ksHeadingSkills'> Skills</span></h1>
+        </div>
+        <div className="carousel-container">
+        {/* Image carousel */}
+        <div className="carousel">
+            <img
+            src={images[currentIndex]}
+            alt={`Slide ${currentIndex + 1}`}
+            className="carousel-image"
+            />
+        </div>
 
-                {/* Indicator circles */}
-                <div className="carousel-indicators">
-                    {images.map((_, index) => (
-                    <div
-                        key={index}
-                        className={`indicator ${currentIndex === index ? 'active' : ''}`}
-                    ></div>
-                    ))}
-                </div>
-            </div>
-
-        </section>
-    )
+        {/* Indicator circles */}
+        <div className="carousel-indicators">
+            {images.map((_, index) => (
+            <div
+                key={index}
+                className={`indicator ${currentIndex === index ? 'active' : ''}`}
+            ></div>
+            ))}
+        </div>
+        </div>
+    </section>
+  );
 }
 
 export default KeySkills
